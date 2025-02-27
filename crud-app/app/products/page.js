c499e2ed13b21370a0f0a84d9c39f475089d34e7
Aanshi R kumar <96661837+annz-gif@users.noreply.github.com>
2025-02-27 21:50:18 +0530

    Update page.js

--- crud-app/app/products/page.js
+++ crud-app/app/products/page.js
@@ -2,6 +2,21 @@ import ProductsList from "@/components/ProductsList";
 
 
 
-export default function Home(){
-    return <ProductsList/>;
+export const dynamic = "force-dynamic";
+
+export default async function ProductsPage() {
+  const res = await fetch('http://localhost:3000/api/products', { cache: 'no-store' });
+  const { products } = await res.json();
+
+  return (
+    <div>
+      <h1>Products List</h1>
+      {products.map((product) => (
+        <div key={product._id}>
+          <h2>{product.name}</h2>
+          <p>Price: ${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
